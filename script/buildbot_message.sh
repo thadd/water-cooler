2dff345020fa1915c8e9d259779f9ed2174afce3
Thadd Selden <thadd.selden@gmail.com>
2008-11-30 23:00:09 -0500

    Removing extraneous scripts

--- script/buildbot_message.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-script/runner -e production "Message.create(:participant => Participant.find_by_username('autobuilder'), :chat_room => ChatRoom.find_by_name('Builds'), :content => '$1')"
